d74175acc2cdc498714406a77d0b8855e7f9ae5a
GavinGroves <649941598@qq.com>
2023-04-26 01:01:28 +0800

    常量工具类

---
+++ Utilities/Settings.cs
@@ -0,0 +1,10 @@
+/// <summary>
+/// 常量工具类
+/// </summary>
+public class Settings
+{
+    //渐变时间
+    public const float fadeDuration = 0.35f;
+    //渐变程度
+    public const float targetAlpha = 0.45f;
+}
